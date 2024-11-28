7ac1947e2e2640b41cfe28dd34dfaf0628cf74de
CJ Karkut <cjkarkut@gmail.com>
2024-11-27 21:18:44 -0600

    add redux persisting to local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,43 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
 
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+};
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedCartReducer = persistReducer(cartPersistConfig, cartReducer);
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
-    auth: authReducer
+    cart: persistedCartReducer,
+    auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/PAUSE",
+          "persist/FLUSH",
+          "persist/PURGE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
